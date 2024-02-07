2c0f74b0e65bad0a924b3dadc171d4a90bd877c9
khalid-kifayat <khalid.kifayat@gmail.com>
2024-02-07 17:20:51 +0500

    commit

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF's using Gemini-Pro Model💁")
+    st.header("Chat with PDF's 🤖 Gemini-Pro Model💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
