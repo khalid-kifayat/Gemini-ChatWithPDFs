0fd5be695dc984368bee568add5da8c22c1ec448
khalid-kifayat <khalid.kifayat@gmail.com>
2024-03-08 08:17:00 +0500

    commit

--- app.py
+++ app.py
@@ -94,10 +94,24 @@ def main():
 if __name__ == "__main__":
     main()
 
-
+# Footer
+st.markdown("""
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #000000; /* Black background color */
+    color: #ffffff; /* White text color */
+    text-align: center;
+    padding: 10px;
+}
+</style>
+""", unsafe_allow_html=True)
 
 st.markdown("---")
-st.caption("using Google Gemini Pro, Python-Langchain Application created by Khalid kifayat :sunglasses:")
+st.markdown('<p class="footer">Generative AI : Python-Langchain Application <br>  Created by Khalid Kifayat <br> (www.beingkhalid.com / www.builtautomations.com)</p>', unsafe_allow_html=True)
 
 hide_streamlit_style = """
             <style>
